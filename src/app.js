27b37c30356f81448f11776260f7f7070f6e6c08
Pandiaraj <pandiarajsppr@gmail.com>
2026-02-27 15:51:01 +0800

    Basic API creations

--- src/app.js
+++ src/app.js
@@ -2,10 +2,25 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello", (req, res) => {
-    res.send("Hello from Pandi!!");
-});
+// app.use("/hello", (req, res) => {
+//     res.send("Hello from Pandi!!");
+// });
 
+app.get("/user", (req, res) => {
+    res.send("GET api called");
+});
+app.post("/user", (req, res) => {
+    res.send("POST api called");
+})
+app.put("/user", (req, res) => {
+    res.send("PUT api called");
+})
+app.patch("/user", (req, res) => {
+    res.send("PATCH api called");
+})
+app.delete("/user", (req, res) => {
+    res.send("DELETE api called");
+})
 app.listen(3000, () => {
     console.log("Server running in the port - 3000");
-});
+}); 
