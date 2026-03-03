1f27aa482b9fff444be0f2427b4b30ac1c797ddc
Pandiaraj <pandiarajsppr@gmail.com>
2026-03-03 15:35:32 +0800

    DB connection and basic POST

--- src/app.js
+++ src/app.js
@@ -1,16 +1,31 @@
 const express = require("express");
 const { userAuth } = require("./midldewares/auth");
+const connectDB = require("./config/database");
+const User = require("./models/user");
 const app = express();
 
-// app.use("/", userAuth);
+app.post("/signup", async (req, res) => {
+    const user = new User({
+        firstName: "Pandiaraj",
+        lastName: "SP",
+        emailId: "pandi@gmail.com",
+        password: 21234
+    });
 
-app.get("/user/allData", userAuth, (req, res) => {
-    res.send("GET called");
-})
-app.get("/user/someData", (req, res) => {
-    res.send("GET Called for user someData");
+    try {
+        await user.save();
+        res.send("User saved");
+    } catch (err) {
+        res.status(400).send("Error saving the user: " + err.message)
+    }
 })
 
-app.listen(3000, () => {
-    console.log("Server running in the port - 3000");
-}); 
+connectDB().then(() => {
+    console.log("DB connection established");
+    app.listen(3000, () => {
+        console.log("Server running in the port - 3000");
+    });
+}).catch((err) => {
+    console.error("DB connection failed! : ", err);
+})
+
