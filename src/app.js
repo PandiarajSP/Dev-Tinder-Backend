42d2e878e67ddf58594090480b794a563a8621fb
Pandiaraj <pandiarajsppr@gmail.com>
2026-03-03 16:27:47 +0800

    Dive deep into APIs I

--- src/app.js
+++ src/app.js
@@ -3,20 +3,51 @@ const { userAuth } = require("./midldewares/auth");
 const connectDB = require("./config/database");
 const User = require("./models/user");
 const app = express();
+// Middleware for parsing the request into JSON
+app.use(express.json())
 
 app.post("/signup", async (req, res) => {
-    const user = new User({
-        firstName: "Pandiaraj",
-        lastName: "SP",
-        emailId: "pandi@gmail.com",
-        password: 21234
-    });
+    const request = req.body;
+    const user = new User(request);
 
     try {
         await user.save();
         res.send("User saved");
     } catch (err) {
         res.status(400).send("Error saving the user: " + err.message)
+    }
+})
+
+// Get user by email
+app.get("/user", async (req, res) => {
+    const email = req.body.emailId;
+
+    try {
+        // Find only one user for this email (Two users with same email - it returns the one (arbitrary))
+        // const result = await User.findOne({ emailId: email });
+
+
+        // Find users based on email
+        const result = await User.find({ emailId: email });
+        if (result.length === 0) {
+            res.status(404).send("User not found");
+        }
+        res.send(result);
+    } catch (err) {
+        res.status(400).send("Something went wrong - " + err.message)
+    }
+})
+
+// Feed API - GET /feed - get all the users from the database
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find({});
+        if (users.length === 0) {
+            res.status(404).send("No users found");
+        }
+        res.send(users);
+    } catch (err) {
+        res.status(500).send("Something went wrong");
     }
 })
 
