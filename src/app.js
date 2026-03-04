325a2baec6443d2fee7e427b5f86b1676ccdd149
Pandiaraj <pandiarajsppr@gmail.com>
2026-03-04 16:22:42 +0800

    Data validation in shcema level and api level

--- src/app.js
+++ src/app.js
@@ -65,7 +65,7 @@ app.patch("/user", async (req, res) => {
     const userId = req.body.userId
     const data = req.body;
     try {
-        const user = await User.findByIdAndUpdate({ _id: userId }, data, { returnDocument: "after" });
+        const user = await User.findByIdAndUpdate({ _id: userId }, data, { returnDocument: "after", runValidators: true });
         res.send(user);
     } catch (err) {
         res.status(400).send("Something went wrong");
--- src/models/user.js
+++ src/models/user.js
@@ -1,30 +1,72 @@
 const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-    firstName: {
-        type: String
-    },
-    lastName: {
-        type: String
-    },
-    emailId: {
-        type: String
-    },
-    password: {
-        type: String
-    },
-    age: {
-        type: Number
-    },
-    gender: {
-        type: String
-    },
-    city: {
-        type: String
-    },
-    phoneNumber: {
-        type: Number
-    }
-})
+const validate = require("validator");
+const userSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+            minLength: 4
+        },
+        lastName: {
+            type: String
+        },
+        emailId: {
+            type: String,
+            required: true,
+            lowercase: true,
+            unique: true,
+            trim: true,
+            validate(value) {
+                if (!validate.isEmail(value))
+                    throw new Error("Invalid Email address")
+            }
+        },
+        password: {
+            type: String,
+            required: true,
+            validate(value) {
+                if (!validate.isStrongPassword(value)) {
+                    throw new Error("Enter strong password");
+                }
+            }
+        },
+        age: {
+            type: Number,
+            min: 18
+        },
+        gender: {
+            type: String,
+            // it only runs for creating new document, 
+            // If we need to run this validation while updation, check runValidators in findByIdAndUpdate
+            validate(value) {
+                if (!["male", "female", "others"].includes(value))
+                    throw new Error("Gender data not valid");
+            }
+        },
+        city: {
+            type: String
+        },
+        phoneNumber: {
+            type: Number,
+            validate(value) {
+                if (!validate.isMobilePhone(value)) {
+                    throw new Error("Invalid Phone number");
+                }
+            }
+        },
+        photoUrl: {
+            type: String
+        },
+        about: {
+            type: String,
+            default: "This is the sample default about of the user"
+        },
+        skills: {
+            type: [String]
+        }
+    }, {
+    timestamps: true
+}
+)
 
 module.exports = mongoose.model("User", userSchema);
